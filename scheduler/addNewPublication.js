8eb2e173eedd8a79086363a064156a2d1e63b74a
namroodinc <a_namrood@hotmail.com>
2017-12-16 23:29:15 +0000

    added base url

--- scheduler/addNewPublication.js
+++ scheduler/addNewPublication.js
@@ -1,6 +1,7 @@
 import getApi from "../utils/getApi";
+import { appBaseUrl } from "../config";
 
-const getList = getApi('/api/list/publications');
+const getList = getApi(`${appBaseUrl}/api/list/publications`);
 getList.then((data) => {
   console.log(data);
 });
