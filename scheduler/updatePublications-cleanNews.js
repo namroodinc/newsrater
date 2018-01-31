9ce992200743fc2d316688e4ce34d89574497d23
namroodinc <a_namrood@hotmail.com>
2018-01-31 22:16:57 +0000

    added clean news function to stop there being 1000's of articles clogging up the system

---
+++ scheduler/updatePublications-cleanNews.js
@@ -0,0 +1,38 @@
+import * as contentful from "contentful-management";
+
+const client = contentful.createClient({
+  accessToken: cfCmaToken
+});
+const getSpace = client.getSpace(cfSpaceId);
+
+import { cfSpaceId, cfCmaToken } from "../config";
+const typeOfUpdate = 'Top Headlines for today';
+
+getSpace
+  .then((space) => space.getEntries({
+    content_type: 'publication',
+    order: 'sys.updatedAt'
+  }))
+  .then((response) => {
+    response.items.map(data => {
+
+      getSpace
+        .then((space) => space.getEntry(data.sys.id))
+        .then((entry) => {
+
+          console.log('Before there were: ', entry.fields.articles['en-US'].length, ' news articles');
+          const articleSort = entry.fields.articles['en-US'].sort((a, b) => {
+            return new Date(b.publishedAt) - new Date(a.publishedAt);
+          }).splice(0, 50);
+          console.log('Now there are: ', articleSort.length, ' news articles');
+
+          entry.fields.articles['en-US'] = articleSort;
+          return entry.update();
+        })
+        .then((entry) => entry.publish())
+        .then((entry) => console.log(`** ${entry.fields.name['en-US']} ${typeOfUpdate} cleaned & published.`))
+        .catch(console.error);
+
+    });
+  })
+  .catch(console.error);
