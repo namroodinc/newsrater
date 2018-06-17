37c445198bcaaac9a4b8aa5c6bb111b484eaed0e
namroodinc <a_namrood@hotmail.com>
2018-06-17 23:27:09 +0100

    expanded tags section

--- scheduler/mLabs-getNewsApi.js
+++ scheduler/mLabs-getNewsApi.js
@@ -16,27 +16,42 @@ getSpace
   .then((response) => {
     response.items.map(data => {
       const newsApiId = !data.fields.newsApiId ? null : data.fields.newsApiId['en-US'];
+      const countryCode = !data.fields.countryGeocode ? null : data.fields.countryGeocode['en-US'];
 
-      if (newsApiId) {
+      if (newsApiId && (countryCode === 'GB' || countryCode === 'US' || countryCode === 'AU' || countryCode === 'DE')) {
+        let lang = 'en';
+        if (countryCode === 'DE') {
+          lang = 'de';
+        } else if (countryCode === 'IT') {
+          lang = 'it';
+        }
+
         getApi(`${newsApi}?sources=${newsApiId}&apiKey=${newsApiKey}`)
           .then(results => {
 
             const articles = results.articles.map(article => {
-              const { title } = article;
-              return title;
+              const { publishedAt, title, url } = article;
+              return {
+                publishedAt,
+                title,
+                url
+              }
             });
 
-            const articleTagging = articles.map(title => {
+            const articleTagging = articles.map(article => {
+              const { publishedAt, title, url } = article;
               return new Promise((resolve) => {
-                getApi(`${d4TaggingUrl}${encodeURIComponent(title.trim())}`)
+                getApi(`${d4TaggingUrl}${encodeURIComponent(title.trim())}&lang=${lang}`)
                   .then((data) => {
-                    const results = data
+                    const tags = data
                       .annotations
                       .filter(annotation => annotation['link_probability'] > 0.2);
 
                     resolve({
+                      publishedAt,
                       title,
-                      data: results
+                      url,
+                      tags
                     });
                   });
               });
@@ -45,13 +60,17 @@ getSpace
             return Promise.all(articleTagging)
               .then((values) => {
                 values.map((value) => {
+                  console.log(value.publishedAt);
                   console.log(value.title);
-                  console.log(value.data);
+                  console.log(value.url);
+                  console.log(value.tags);
                   console.log('---------');
                 })
               });
 
           });
+      } else {
+        console.log(newsApiId, ' not annotated as not English/German/Italian');
       }
     });
   })
