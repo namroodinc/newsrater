d3a41084fab6afa3c376f1e5011fcb1936e5d0fd
namroodinc <ashoornamrood@gmail.com>
2018-06-21 10:12:19 +0100

    pt 1 of changes

--- scheduler/mLabs-getNewsApi.js
+++ scheduler/mLabs-getNewsApi.js
@@ -6,72 +6,81 @@ const client = contentful.createClient({
 const getSpace = client.getSpace(cfSpaceId);
 
 import getApi from "../utils/getApi";
+// import postApi from "../utils/postApi";
 import { cfSpaceId, cfCmaToken, d4TaggingUrl, newsApi, newsApiKey } from "../config";
-//, mongoDb, mongoDbApiKey, mongoDbBaseUrl
+// mongoDb, mongoDbApiKey, mongoDbBaseUrl, 
 
 getSpace
   .then((space) => space.getEntries({
     content_type: 'publication'
   }))
   .then((response) => {
-    response.items.map(data => {
-      const newsApiId = !data.fields.newsApiId ? null : data.fields.newsApiId['en-US'];
-      const countryCode = !data.fields.countryGeocode ? null : data.fields.countryGeocode['en-US'];
+    response
+      .items
+      .filter(data => {
+        const countryCode = !data.fields.countryGeocode ? null : data.fields.countryGeocode['en-US'];
+        return (countryCode === 'GB' || countryCode === 'US' || countryCode === 'AU')
+      })
+      .map(data => {
+        const newsApiId = !data.fields.newsApiId ? null : data.fields.newsApiId['en-US'];
 
-      if (newsApiId && (countryCode === 'GB' || countryCode === 'US' || countryCode === 'AU' || countryCode === 'DE')) {
-        let lang = 'en';
-        if (countryCode === 'DE') {
-          lang = 'de';
-        } else if (countryCode === 'IT') {
-          lang = 'it';
-        }
+        if (newsApiId) {
+          const lang = 'en';
+          getApi(`${newsApi}?sources=${newsApiId}&apiKey=${newsApiKey}`)
+            .then(results => {
 
-        getApi(`${newsApi}?sources=${newsApiId}&apiKey=${newsApiKey}`)
-          .then(results => {
+              const articles = results.articles.map(article => {
+                const { publishedAt, title, url } = article;
+                return {
+                  publishedAt,
+                  title,
+                  url
+                }
+              });
 
-            const articles = results.articles.map(article => {
-              const { publishedAt, title, url } = article;
-              return {
-                publishedAt,
-                title,
-                url
-              }
-            });
+              const articleTagging = articles.map(article => {
+                const { publishedAt, title, url } = article;
+                return new Promise((resolve) => {
+                  getApi(`${d4TaggingUrl}${encodeURIComponent(title.trim())}&lang=${lang}`)
+                    .then((data) => {
+                      const trends = data
+                        .annotations
+                        .filter(annotation => annotation['link_probability'] > 0.2);
 
-            const articleTagging = articles.map(article => {
-              const { publishedAt, title, url } = article;
-              return new Promise((resolve) => {
-                getApi(`${d4TaggingUrl}${encodeURIComponent(title.trim())}&lang=${lang}`)
-                  .then((data) => {
-                    const tags = data
-                      .annotations
-                      .filter(annotation => annotation['link_probability'] > 0.2);
-
-                    resolve({
-                      publishedAt,
-                      title,
-                      url,
-                      tags
+                      resolve({
+                        publishedAt,
+                        title,
+                        url,
+                        trends
+                      });
                     });
-                  });
+                });
               });
+
+              return Promise.all(articleTagging)
+                .then((values) => {
+                  values.map((value) => {
+                    console.log(value.title);
+                    // values.trends.map(trend => {
+                    //   return new Promise(resolve => {
+                    //     postApi(
+                    //       `${mongoDbBaseUrl}${mongoDb}/collections/metrics?apiKey=${mongoDbApiKey}`,
+                    //       {
+                    //         'type': 'trend',
+                    //         'body': {
+                    //           'title': trend.title,
+                    //           'description': trend.title
+                    //         }
+                    //       }
+                    //     ).then(values => console.log(values));
+                    //   });
+                    // });
+                    console.log('---------');
+                  })
+                });
+
             });
-
-            return Promise.all(articleTagging)
-              .then((values) => {
-                values.map((value) => {
-                  console.log(value.publishedAt);
-                  console.log(value.title);
-                  console.log(value.url);
-                  console.log(value.tags);
-                  console.log('---------');
-                })
-              });
-
-          });
-      } else {
-        console.log(newsApiId, ' not annotated as not English/German/Italian');
-      }
-    });
+        }
+      });
   })
   .catch(console.error);
